004bb22c1e964cde2b9878e3b0678d79d4341a42
lenville <lenville@gmail.com>
2013-06-11 11:44:35 +0800

    add createSource setvolumn

--- test/js/onlinedj.js
+++ test/js/onlinedj.js
@@ -1,6 +1,5 @@
 window.onload  = init;
 var context = new webkitAudioContext();//建立AudioContext实例,webKit核心的浏览器, 需要加上webkit前缀
-var gainNode = context.createGainNode();// 创建一个gain node
 var bufferLoader;//创建AudioBuffer
 
 /*Web Audio API初始化读取音频文件*/
@@ -16,34 +15,34 @@ function init() {
 	bufferLoader.load();
 }
 
-function finishedLoading(bufferList) {
-	//创建两个实例并同时播放之
-	var source1    = context.createBufferSource();
-	var source2    = context.createBufferSource();
-	source1.buffer = bufferList[0];
-	source2.buffer = bufferList[1];
-	source1.connect(context.destination);
-	source2.connect(context.destination);
-	source1.noteOn(0);
-	source2.noteOn(0);
-	}
-
-function playSound(buffer, time) {
+function createSource(buffer){
 	var source = context.createBufferSource();//创建一个声音源
+	var gainNode = context.createGainNode();
+	var filter = context.createBiquadFilter();
 	source.buffer = buffer;//告诉该源播放何物
-	source.connect(context.destination);//将该源与硬件相连
-	source.noteOn(time);//播放该实例time次
+	source.loop = true;
+	source.connect(filter);
+	filter.connect(gainNode);
+	gainNode.connect(context.destination);//将该源与硬件相连
+	return {
+		source: source,
+		gainNode: gainNode,
+		filter: filter
+	};
 }
 
-function volumn(gainNode){
-	this.value = gainNode.gain.value;
-	this.up = function(value){
-		if(value < 100)	value += 5;
+function finishedLoading(bufferList) {	
+	//创建节点实例
+	var source1 = createSource(bufferList[0]);
+	source1.source
+	source1.filter
+	source1.gainNode
+	source1.noteOn(1);//播放该实例time次
 	}
-	this.down = function(value){
-		if(value > 0)	value -=5;
-	}
-	this.mute = function(value){
-		value = 0;
-	}
+
+
+function setvolumn(gainNode, value){
+	gainNode.gain.value = value;
+
 }
+
