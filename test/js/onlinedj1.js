2b66f8f6c6e75a45b62d6894971315b1f2b01f95
lenville <lenville@gmail.com>
2013-06-11 12:43:05 +0800

    修正

--- test/js/onlinedj1.js
+++ test/js/onlinedj1.js
@@ -8,7 +8,8 @@ context = new webkitAudioContext();
 bufferLoader = new BufferLoader(
 context,
 [
-'music/1.mp3'
+'music/1.mp3',
+'music/2.mp3',
 ],
 finishedLoading
 );
@@ -19,7 +20,12 @@ bufferLoader.load();
 function finishedLoading(bufferList) {
 //创建两个实例并同时播放之
 var source1 = context.createBufferSource();
+var source2 = context.createBufferSource();
 source1.buffer = bufferList[0];
+source2.buffer = bufferList[1];
+
 source1.connect(context.destination);
+source2.connect(context.destination);
 source1.noteOn(0);
+source2.noteOn(0);
 }
