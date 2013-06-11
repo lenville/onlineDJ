04ae19136f00315ca8f84438d0dab09c997b8050
lucien <lucienfeel@gmail.com>
2013-06-11 18:18:26 +0800

    1

--- js/dj.js
+++ js/dj.js
@@ -1,11 +1,14 @@
 var audio;
 var touch_arr = [];
-
+var source;
 //每转一圈所需时间（单位：秒）
 var ptime = 5;
-//定时器控制器
-var p = 1;
+//定时器控制器(1转，0不转)
+var p = 0;
+//擦盘声音音量(0-1.0)
+var rubVolume = 0.5;
 //var a;
+
 var dj = {	
 	//定时器
 	interval: function (fn,time){
@@ -14,13 +17,37 @@ var dj = {
 			setTimeout(arguments.callee, time);
 		}, time)
 	}
+	,audiocontext: function (){
+		//初始化audiocontext
+		try{
+			context = new AudioContext();
+		}catch(e){
+			try{
+				context = new webkitAudioContext();
+			}catch(e){
+				alert('Web Audio API is not supported in this browser.');
+			}
+		}
+	}
 	,startMusic: function (name){
+		p = 1;
 		if(!audio)
 			audio = new Audio('music/'+name+'.mp3');
 		else 
 			audio.src = 'music/'+name+'.mp3';
-		audio.addEventListener('canplaythrough', function(){
-			audio.play();			
+			audio.addEventListener('canplaythrough', function(){
+			audio.play();		
+		},false)
+		audio.addEventListener('ended', function(){
+			p = 0;
+		})
+		
+	}
+	,startRubMusic: function(){
+		var rubAudio = new Audio('music/rub.wav');
+		rubAudio.volume = rubVolume;
+		rubAudio.addEventListener('canplaythrough', function(){
+			rubAudio.play();			
 		},false)
 	}
 	,startRub: function(){
@@ -37,22 +64,28 @@ var dj = {
 		var baseX , baseY
 		,height = $('.cover')[0].clientHeight
 		,width = $('.cover')[0].clientWidth
-		,reg = /\-?[0-9]+\.?[0-9]*/g ;
+		,reg = /\-?[0-9]+\.?[0-9]*/g 
+		that = this
 
-		/*
+
+		//触控版的绑定
 		document.addEventListener('touchstart',function(){
-			baseX = event.touches[0].clientX;
-			baseY = event.touches[0].clientY;
-			//if(x>300&&x<600&&y>300&&y<600){
+			baseX = event.touches[0].clientX - $('.table')[0].offsetLeft;
+			baseY = event.touches[0].clientY - $('.table')[0].offsetTop;
+			if(audio&&!audio.ended){
 				p =0;
 				audio.pause();
-				touch_arr = [];
-				touch_arr.push([baseX,baseY]);
-			//}
+			}
+			touch_arr = [];
+			touch_arr.push([baseX,baseY]);
+			
 		},false)
 		document.addEventListener('touchmove',function(){
-			var x = event.changedTouches[0].clientX
-			,y = event.changedTouches[0].clientY;
+			//播放rubmusic
+			that.startRubMusic();
+
+			var x = event.changedTouches[0].clientX - $('.table')[0].offsetLeft
+			,y = event.changedTouches[0].clientY - $('.table')[0].offsetTop;
 			touch_arr.push([x,y])
 			var len = touch_arr.length - 2
 			//console.log(len);
@@ -78,6 +111,7 @@ var dj = {
 				//右上角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_1;
 					//console.log(audio.currentTime);
 				}
@@ -87,6 +121,7 @@ var dj = {
 				//右下角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_2;
 					//console.log(audio.currentTime);
 				}
@@ -96,6 +131,7 @@ var dj = {
 				//左下角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_3;
 					//console.log(audio.currentTime);
 				}
@@ -104,6 +140,7 @@ var dj = {
 				//左上角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_4;
 					//console.log(audio.currentTime);
 				}
@@ -111,27 +148,31 @@ var dj = {
 			//document.write(x+'<br>'+y+'<br>'+audio.currentTime);
 		},false)
 		document.addEventListener('touchend',function(){
-			p = 1;
-			audio.play();
+			if(audio&&!audio.ended){
+				p = 1;
+				audio.play();
+			}
 		},false)
-		*/
-		
-		
 
+
+		//电脑版的绑定
 		$('.cover').bind('mousedown',function(){
 			baseX = event.clientX - $('.table')[0].offsetLeft;
 			baseY = event.clientY - $('.table')[0].offsetTop;
-			//console.log(event.clientX);
-			//if(x>300&&x<600&&y>300&&y<600){
+			if(audio&&!audio.ended){
 				p = 0;
 				audio.pause();
-				touch_arr = [];
-				touch_arr.push([baseX,baseY]);
-			//}
+			}
+			touch_arr = [];
+			touch_arr.push([baseX,baseY]);
+			
 			$('.cover').bind('mousemove', turn);
 		})
 		
 		function turn (ev){
+			//播放rubmusic
+			that.startRubMusic();
+
 			var x = ev.clientX - $('.table')[0].offsetLeft
 			,y = ev.clientY - $('.table')[0].offsetTop;
 			touch_arr.push([x,y])
@@ -159,6 +200,7 @@ var dj = {
 				//右上角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_1;
 					//console.log(audio.currentTime);
 				}
@@ -168,6 +210,7 @@ var dj = {
 				//右下角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_2;
 					//console.log(audio.currentTime);
 				}
@@ -177,6 +220,7 @@ var dj = {
 				//左下角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_3;
 					//console.log(audio.currentTime);
 				}
@@ -185,6 +229,7 @@ var dj = {
 				//左上角
 				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
 				if(len%8 == 1){
+					if(audio&&!audio.ended)
 					audio.currentTime += pchtime_4;
 					//console.log(audio.currentTime);
 				}
@@ -193,17 +238,22 @@ var dj = {
 		}
 		
 		$('.cover').bind('mouseup',function(){
-			p = 1;
-			audio.play();
+			if(audio&&!audio.ended){
+				p = 1;
+				audio.play();
+			}
+			
 			$('.cover').unbind('mousemove', turn);
 		})
 	}
+	//拖动事件
 	,drag: function(){
 		var that = this;
 		$('.musiclist').bind('dragstart',function(){
-			//a=this;
-			//console.log(this.childNodes[5].value);
 			event.dataTransfer.setData('Text',this.childNodes[5].value);
+		})
+		$('.musiclist').bind('click',function(){
+			that.startMusic(this.childNodes[5].value);
 		})
 		$('.cover').bind('dragover',function(){
 			event.preventDefault();
@@ -217,12 +267,76 @@ var dj = {
 			//console.log(data);
 		})
 	}
+	//控制区域事件
+	,control: function(){
+		var that = this
+		,filter_arr = [	["lowpass",0], 
+					  ["highpass",0],
+					  ["bandpass",0],
+					  ["lowshelf",0],   
+					  ["highshelf",0],
+					  ["peaking",0],
+					  ["notch",0],
+					  ["allpass",0]];
+		//开始暂停
+			$('#play').bind('click',function(){
+				if(!audio){
+					that.startMusic('1');
+					this.value = "暂停";
+				}
+				else{
+					if(audio.paused){
+						audio.play();
+						this.value = '暂停';
+						p = 1;
+					}
+					else{
+						audio.pause();
+						this.value = '开始';
+						p = 0;
+					}
+				}
+				
+			})
+		//停止
+			$('#stop').bind('click',function(){			
+				audio.pause();
+				audio.currentTime = 0;
+				$('#play').val('开始');
+				p = 0;
+			})
+
+		//音乐音量调节
+		$('#volume').bind('change', function(){
+			audio.volume = this.value/100;
+		})
+		//擦盘音量调节
+
+		$('#rubvolume').bind('change', function(){
+			rubVolume = this.value/100;
+		})
+		//滤波器
+		function filter (arr){
+			for (var i = 0; i < 8; i++) {
+				$('#'+arr[i][0]).bind('click', function(){
+					arr[i][1] = 1?0:1;
+				})
+			}
+		}(filter_arr);
+		$('#frequency').bind('change', function(){
+			var frequency_arr = [220,440,880,1600,3200,6400];
+			source.setfilter.frequency(frequency_arr[this.value])
+		})
+
+	}
 	,init: function(){
 		$('body')[0].style.height = document.documentElement.clientHeight;
-		this.startMusic('1');
+		//this.startMusic('1');
 		this.startRub();
 		this.drag();
 		this.rub();
+		this.control();
+		web_audio_api_init();
 	}
 }
 dj.init();
