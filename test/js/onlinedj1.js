4ff96c1cf3985d42104fe75a02a5475d289b096b
lenville <lenville@gmail.com>
2013-06-11 13:31:29 +0800

    修正

--- test/js/onlinedj1.js
+++ test/js/onlinedj1.js
@@ -1,90 +1,70 @@
-var context = 0;
-var compressor = 0;
-var reverb = 0;
+<!DOCTYPE html>
+<!-- Author: Eric Bidelman (ericbidelman@chromium.org) -->
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+  <title>Web Audio API: Simple load + play</title>
+</head>
+<body>
+  <p>Example of using the Web Audio API to load a sound file
+  and start playing on user-click.</p>
+  <input type="file" accept="audio/*">
+  <button onclick="playSound()" disabled>Start</button>
+  <button onclick="stopSound()" disabled>Stop</button>
+<script>
+var context = new window.webkitAudioContext();
+var source = null;
+var audioBuffer = null;
 
-var source1 = 0;
-var source2 = 0;
-var source3 = 0;
-
-var lowpassFilter = 0;
-var waveShaper = 0;
-var panner = 0;
-
-var dry1 = 0;
-var dry2 = 0;
-var dry3 = 0;
-
-var wet1 = 0;
-var wet2 = 0;
-var wet3 = 0;
-
-var masterDry = 0;
-var masterWet = 0;
-
-window.onload = setupRoutingGraph;
-
-function setupRoutingGraph () {
-    context = new webkitAudioContext();
-
-    // Create the effects nodes.
-    lowpassFilter = context.createBiquadFilter();
-    waveShaper = context.createWaveShaper();
-    panner = context.createPanner();
-    compressor = context.createDynamicsCompressor();
-    reverb = context.createConvolver();
-
-    // Create master wet and dry.
-    masterDry = context.createGainNode();
-    masterWet = context.createGainNode();
-
-    // Connect final compressor to final destination.
-    compressor.connect(context.destination);
-
-    // Connect master dry and wet to compressor.
-    masterDry.connect(compressor);
-    masterWet.connect(compressor);
-
-    // Connect reverb to master wet.
-    reverb.connect(masterWet);
-
-    // Create a few sources.
-    source1 = context.createBufferSource();
-    source2 = context.createBufferSource();
-    source3 = context.createOscillator();
-
-    source1.buffer = manTalkingBuffer;
-    source2.buffer = footstepsBuffer;
-    source3.frequency.value = 440;
-
-    // Connect source1
-    dry1 = context.createGainNode();
-    wet1 = context.createGainNode();
-    source1.connect(lowpassFilter);
-    lowpassFilter.connect(dry1);
-    lowpassFilter.connect(wet1);
-    dry1.connect(masterDry);
-    wet1.connect(reverb);
-
-    // Connect source2
-    dry2 = context.createGainNode();
-    wet2 = context.createGainNode();
-    source2.connect(waveShaper);
-    waveShaper.connect(dry2);
-    waveShaper.connect(wet2);
-    dry2.connect(masterDry);
-    wet2.connect(reverb);
-
-    // Connect source3
-    dry3 = context.createGainNode();
-    wet3 = context.createGainNode();
-    source3.connect(panner);
-    panner.connect(dry3);
-    panner.connect(wet3);
-    dry3.connect(masterDry);
-    wet3.connect(reverb);
-    
-    // Start the sources now.
-    source1.start(0);
-    source2.start(0);
-    source3.start(0);
+function stopSound() {
+  if (source) {
+    source.noteOff(0);
+  }
 }
+
+function playSound() {
+  // source is global so we can call .noteOff() later.
+  source = context.createBufferSource();
+  source.buffer = audioBuffer;
+  source.loop = false;
+  source.connect(context.destination);
+  source.noteOn(0); // Play immediately.
+}
+
+function initSound(arrayBuffer) {
+  context.decodeAudioData(arrayBuffer, function(buffer) {
+    // audioBuffer is global to reuse the decoded audio later.
+    audioBuffer = buffer;
+    var buttons = document.querySelectorAll('button');
+    buttons[0].disabled = false;
+    buttons[1].disabled = false;
+  }, function(e) {
+    console.log('Error decoding file', e);
+  }); 
+}
+
+// User selects file, read it as an ArrayBuffer and pass to the API.
+var fileInput = document.querySelector('input[type="file"]');
+fileInput.addEventListener('change', function(e) {  
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    initSound(this.result);
+  };
+  reader.readAsArrayBuffer(this.files[0]);
+}, false);
+
+// Load file from a URL as an ArrayBuffer.
+// Example: loading via xhr2: loadSoundFile('sounds/test.mp3');
+function loadSoundFile(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function(e) {
+    initSound(this.response); // this.response is an ArrayBuffer.
+  };
+  xhr.send();
+}
+</script>
+</body>
+</html>
