dba8f9088abd69e58df8047eec0e1a34847a5045
lenville <lenville@gmail.com>
2013-06-11 12:53:43 +0800

    修正

--- test/js/onlinedj1.js
+++ test/js/onlinedj1.js
@@ -1,32 +1,88 @@
-window.onload = init;
-var context;
-var bufferLoader;
+var context = 0;
+var compressor = 0;
+var reverb = 0;
 
-function init() {
-context = new webkitAudioContext();
+var source1 = 0;
+var source2 = 0;
+var source3 = 0;
 
-bufferLoader = new BufferLoader(
-context,
-[
-'music/1.mp3',
-'music/2.mp3',
-],
-finishedLoading
-);
+var lowpassFilter = 0;
+var waveShaper = 0;
+var panner = 0;
 
-bufferLoader.load();
-}
+var dry1 = 0;
+var dry2 = 0;
+var dry3 = 0;
 
-function finishedLoading(bufferList) {
-//创建两个实例并同时播放之
-console('123');
-var source1 = context.createBufferSource();
-var source2 = context.createBufferSource();
-source1.buffer = bufferList[0];
-source2.buffer = bufferList[1];
+var wet1 = 0;
+var wet2 = 0;
+var wet3 = 0;
 
-source1.connect(context.destination);
-source2.connect(context.destination);
-source1.noteOn(0);
-source2.noteOn(0);
+var masterDry = 0;
+var masterWet = 0;
+
+function setupRoutingGraph () {
+    context = new AudioContext();
+
+    // Create the effects nodes.
+    lowpassFilter = context.createBiquadFilter();
+    waveShaper = context.createWaveShaper();
+    panner = context.createPanner();
+    compressor = context.createDynamicsCompressor();
+    reverb = context.createConvolver();
+
+    // Create master wet and dry.
+    masterDry = context.createGainNode();
+    masterWet = context.createGainNode();
+
+    // Connect final compressor to final destination.
+    compressor.connect(context.destination);
+
+    // Connect master dry and wet to compressor.
+    masterDry.connect(compressor);
+    masterWet.connect(compressor);
+
+    // Connect reverb to master wet.
+    reverb.connect(masterWet);
+
+    // Create a few sources.
+    source1 = context.createBufferSource();
+    source2 = context.createBufferSource();
+    source3 = context.createOscillator();
+
+    source1.buffer = manTalkingBuffer;
+    source2.buffer = footstepsBuffer;
+    source3.frequency.value = 440;
+
+    // Connect source1
+    dry1 = context.createGainNode();
+    wet1 = context.createGainNode();
+    source1.connect(lowpassFilter);
+    lowpassFilter.connect(dry1);
+    lowpassFilter.connect(wet1);
+    dry1.connect(masterDry);
+    wet1.connect(reverb);
+
+    // Connect source2
+    dry2 = context.createGainNode();
+    wet2 = context.createGainNode();
+    source2.connect(waveShaper);
+    waveShaper.connect(dry2);
+    waveShaper.connect(wet2);
+    dry2.connect(masterDry);
+    wet2.connect(reverb);
+
+    // Connect source3
+    dry3 = context.createGainNode();
+    wet3 = context.createGainNode();
+    source3.connect(panner);
+    panner.connect(dry3);
+    panner.connect(wet3);
+    dry3.connect(masterDry);
+    wet3.connect(reverb);
+    
+    // Start the sources now.
+    source1.start(0);
+    source2.start(0);
+    source3.start(0);
 }
