0444ab385a9fe2438486943da642f82012f0420a
lenville <lenville@gmail.com>
2013-06-11 12:24:08 +0800

    修正

---
+++ test/js/onlinedj1.js
@@ -0,0 +1,25 @@
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+context = new webkitAudioContext();
+
+bufferLoader = new BufferLoader(
+context,
+[
+'music/1.mp3'
+],
+finishedLoading
+);
+
+bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+//创建两个实例并同时播放之
+var source1 = context.createBufferSource();
+source1.buffer = bufferList[0];
+source1.connect(context.destination);
+source1.noteOn(0);
+}
