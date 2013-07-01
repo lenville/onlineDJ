32be0c7de460f0540261adec1aae52a496f18c31
lenville <lenville@gmail.com>
2013-07-01 20:08:04 +0800

    push new demo(fix play bugs)

--- demo/README.md
+++ demo/README.md
@@ -1,7 +1,10 @@
 OnlineDJ
 ========
 
-详情请见[Demo](http://xlucien.net/onlineDJ/)  
+Collaborator  
+[Lucien](https://github.com/lucienthink)
+
+详情请见[Demo](http://xlucien.net/project/onlineDJ/)  
 
 如若要访问Demo, 请遵循以下指引  
 + 滤波器的功能在页面及音乐加载完成时打开开关调整相应参数进行测试(只对开始加载的音源绑定了相应的Filter节点)  
--- demo/js/dj.js
+++ demo/js/dj.js
@@ -1,405 +1,81 @@
 var audio;
-var touch_arr = [];
+//父窗口的url
+var url = window.top.location.href+'index.html';
+//滤波器对象
 var source;
 //每转一圈所需时间（单位：秒）
 var ptime = 5;
-//定时器控制器(1转，0不转)
-var p = 0;
-//擦盘声音音量(0-1.0)
-var rubVolume = 0.5;
 //停止控制
 var stop = 1;
-//var a;
-var dj = {	
-	//定时器
-	interval: function (fn,time){
-		setTimeout(function(){
-			fn();
-			setTimeout(arguments.callee, time);
-		}, time)
-	}
+var dj = {
+
 	//音乐加载
-	,startMusic: function (name){
-		
-		if(!audio)
-			audio = document.querySelector('audio');
-		else{
-			delete audio;
-			audio = null;
-			$('#au').html('');
-			$('#au').html('<audio src="music/'+name+'.mp3"></audio>');
-			audio = document.querySelector('audio');
-			
-		}
+	startMusic: function (name){
+		audio = document.querySelector('audio');
 		//绑定加载完开始
 		audio.addEventListener('canplaythrough', function(){
-			if(stop == 0){
-				p = 1;
-				audio.play();
-			}		
+			//发送到父窗口，通知已加载完，父窗口控制开始播放
+			window.top.postMessage('ready',url);
 		},false)
 		//绑定播放完停止
 		audio.addEventListener('ended', function(){
 			stop = 1;
-			p = 0;
-			$('#play')[0].value = '开始';
-		})
-		
-	}
-	//擦盘音乐加载
-	,startRubMusic: function(){
-		var rubAudio = new Audio('music/rub.wav');
-		rubAudio.volume = rubVolume;
-		rubAudio.addEventListener('canplaythrough', function(){
-			rubAudio.play();			
+			window.top.postMessage('stop',url);
 		},false)
-	}
-	//擦盘加载
-	,startRub: function(){
-		this.interval(fn,30);
-		var pdeg = 360/(ptime/0.03)
-		,reg = /\-?[0-9]+\.?[0-9]*/g ;
-		function fn (){
-			var rotate = $('.logo')[0].style.msTransform||$('.logo')[0].style.webkitTransform;
-			rotate = (rotate&&rotate.match(reg)) ? rotate.match(reg)[0] : 0 ;
-			if(p == 1){
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg)+'deg)';
-			}
-		}
-	}
-	//擦盘实现
-	,rub: function() {
-		var baseX , baseY
-		,height = $('.cover')[0].clientHeight
-		,width = $('.cover')[0].clientWidth
-		,reg = /\-?[0-9]+\.?[0-9]*/g 
-		that = this;
-
-
-		//触控版的擦盘实现
-		document.addEventListener('touchstart',function(){
-			baseX = event.touches[0].clientX - $('.table')[0].offsetLeft;
-			baseY = event.touches[0].clientY - $('.table')[0].offsetTop;
-			if(audio&&!audio.ended){
-				p =0;
-				audio.pause();
-			}
-			touch_arr = [];
-			touch_arr.push([baseX,baseY]);
-			
-		},false)
-		document.addEventListener('touchmove',function(){
-			//播放rubmusic
-			that.startRubMusic();
-
-			var x = event.changedTouches[0].clientX - $('.table')[0].offsetLeft
-			,y = event.changedTouches[0].clientY - $('.table')[0].offsetTop;
-			touch_arr.push([x,y])
-			var len = touch_arr.length - 2
-			//console.log(len);
-			,rotate = $('.logo')[0].style.msTransform||$('.logo')[0].style.webkitTransform;
-			rotate = (rotate&&rotate.match(reg)) ? rotate.match(reg)[0] : 0 ;
-			//每次转动角度
-			var pdeg_1 = (x-touch_arr[len][0]+y-touch_arr[len][1])/7
-			,pdeg_2 = (-x+touch_arr[len][0]+y-touch_arr[len][1])/7
-			,pdeg_3 = (-x+touch_arr[len][0]-y+touch_arr[len][1])/7
-			,pdeg_4 = (x-touch_arr[len][0]-y+touch_arr[len][1])/7
-			//每次改变音轨时间长度
-			,pchtime_1 = ptime*(pdeg_1/360)*12
-			,pchtime_2 = ptime*(pdeg_2/360)*12
-			,pchtime_3 = ptime*(pdeg_3/360)*12
-			,pchtime_4 = ptime*(pdeg_4/360)*12;
-
-			//audio.currentTime += x/100;
-			//audio.currentTime += y/100;
-
-			//判定擦盘位置，旋转擦盘，改变音轨
-			//console.log(x+'<br>'+y);
-			
-			if((touch_arr[len][0]>width/2)&&(touch_arr[len][1]<height/2)){
-				//右上角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_1;
-					//console.log(audio.currentTime);
-				}
-				
-			}
-			else if((touch_arr[len][0]>width/2)&&(touch_arr[len][1]>height/2)){
-				//右下角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_2;
-					//console.log(audio.currentTime);
-				}
-				
-			}
-			else if((touch_arr[len][0]<width/2)&&(touch_arr[len][1]>height/2)){
-				//左下角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_3;
-					//console.log(audio.currentTime);
-				}
-			}
-			else if((touch_arr[len][0]<width/2)&&(touch_arr[len][1]<height/2)){
-				//左上角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_4;
-					//console.log(audio.currentTime);
-				}
-			}
-			//document.write(x+'<br>'+y+'<br>'+audio.currentTime);
-		},false)
-		document.addEventListener('touchend',function(){
-			if(audio&&!audio.ended){
-				if(stop == 0){
-					p = 1;
-					audio.play();
-				}
-			}
-		},false)
-
-
-		//电脑版的擦盘实现
-		$('.cover').bind('mousedown',function(){
-			baseX = event.clientX - $('.table')[0].offsetLeft;
-			baseY = event.clientY - $('.table')[0].offsetTop;
-			if(audio&&!audio.ended){
-				p = 0;
-				audio.pause();
-			}
-			touch_arr = [];
-			touch_arr.push([baseX,baseY]);
-			
-			$('.cover').bind('mousemove', turn);
-		})
-		
-		function turn (ev){
-			//播放rubmusic
-			that.startRubMusic();
-
-			var x = ev.clientX - $('.table')[0].offsetLeft
-			,y = ev.clientY - $('.table')[0].offsetTop;
-			touch_arr.push([x,y])
-			var len = touch_arr.length - 2
-			//console.log(len);
-			,rotate = $('.logo')[0].style.msTransform||$('.logo')[0].style.webkitTransform;
-			rotate = (rotate&&rotate.match(reg)) ? rotate.match(reg)[0] : 0 ;
-			//每次转动角度
-			var pdeg_1 = (x-touch_arr[len][0]+y-touch_arr[len][1])/7
-			,pdeg_2 = (-x+touch_arr[len][0]+y-touch_arr[len][1])/7
-			,pdeg_3 = (-x+touch_arr[len][0]-y+touch_arr[len][1])/7
-			,pdeg_4 = (x-touch_arr[len][0]-y+touch_arr[len][1])/7
-			//每次改变音轨时间长度
-			,pchtime_1 = ptime*(pdeg_1/360)*12
-			,pchtime_2 = ptime*(pdeg_2/360)*12
-			,pchtime_3 = ptime*(pdeg_3/360)*12
-			,pchtime_4 = ptime*(pdeg_4/360)*12;
-
-			//audio.currentTime += x/100;
-			//audio.currentTime += y/100;
-
-			//判定擦盘位置，旋转擦盘，改变音轨
-			//console.log(x+'<br>'+y);
-			
-			if((touch_arr[len][0]>width/2)&&(touch_arr[len][1]<height/2)){
-				//右上角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_1)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_1;
-					//console.log(audio.currentTime);
-				}
-				
-			}
-			else if((touch_arr[len][0]>width/2)&&(touch_arr[len][1]>height/2)){
-				//右下角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_2)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_2;
-					//console.log(audio.currentTime);
-				}
-				
-			}
-			else if((touch_arr[len][0]<width/2)&&(touch_arr[len][1]>height/2)){
-				//左下角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_3)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_3;
-					//console.log(audio.currentTime);
-				}
-			}
-			else if((touch_arr[len][0]<width/2)&&(touch_arr[len][1]<height/2)){
-				//左上角
-				$('.logo')[0].style.msTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
-				$('.logo')[0].style.webkitTransform = 'rotate('+(parseInt(rotate)+pdeg_4)+'deg)';
-				if(len%8 == 1){
-					if(audio&&!audio.ended)
-					audio.currentTime += pchtime_4;
-					//console.log(audio.currentTime);
-				}
-			}
-			//document.write(x+'<br>'+y+'<br>'+audio.currentTime);
-		}
-		
-		$('.cover').bind('mouseup',function(){
-			if(audio&&!audio.ended){
-				if(stop == 0){
-					p = 1;
-					audio.play();
-				}
-			}
-			$('.cover').unbind('mousemove', turn);
-		})
-	}
-	//拖动事件
-	,drag: function(){
-		var that = this;
-		//开始移出时信息传递
-		$('.musiclist').bind('dragstart',function(){
-			event.dataTransfer.setData('Text',this.childNodes[5].value);
-		})
-		//取消唱碟机区域的可拖拽事件
-		$('.cover').bind('dragover',function(){
-			event.preventDefault();
-		})
-		$('.cover').bind('dragenter',function(){
-			event.preventDefault();
-		})
-		//拖拽到达唱碟机区域时切换歌曲
-		$('.cover').bind('drop',function(){
-			var data = event.dataTransfer.getData('Text');
-			stop = 0;
-			$('.logo').css('background','url(image/'+data+'.png)');
-			that.startMusic(data);
-			//web_audio_api_init();
-			$('#play').val('暂停');
-		})
-		//点击也可切换歌曲的绑定
-		$('.musiclist').bind('click',function(){
-			stop = 0;
-			$('.logo').css('background','url(image/'+this.childNodes[5].value+'.png)');
-			that.startMusic(this.childNodes[5].value);
-			//web_audio_api_init();
-			$('#play').val('暂停');
-		})
 	}
 	//控制区域事件
 	,control: function(){
-		var that = this
-		,filter_arr = [["lowpass",0],["highpass",0],["bandpass",0],["lowshelf",0],["highshelf",0],["peaking",0],["notch",0],["allpass",0]];
-		a = filter_arr;
-		//开始暂停
-			$('#play').bind('click',function(){
-				if(!audio){
-					that.startMusic('1');
-					this.value = "暂停";
-				}
-				else{
-					if(audio.paused){
-						stop = 0;
-						audio.play();
-						this.value = '暂停';
-						p = 1;
-					}
-					else{
-						stop = 1;
-						audio.pause();
-						this.value = '开始';
-						p = 0;
-					}
-				}
-				
-			})
-		//停止
-			$('#stop').bind('click',function(){			
-				audio.pause();
-				stop = 1;
-				audio.currentTime = 0;
-				$('#play').val('开始');
-				p = 0;
-			})
-
-		//音乐音量调节
-		$('#volume').bind('change', function(){
-			audio.volume = this.value/100;
-		})
-		//擦盘音量调节
-
-		$('#rubvolume').bind('change', function(){
-			rubVolume = this.value/100;
-		})
-
-		//滤波器
-		//开
-		$('#filter-on').bind('click', function(){
-			source.addfilter();
-			$('#filter-on')[0].disabled = true;
-			$('#filter-off')[0].disabled = false;
-			source.setfilter.type(filter_arr[7][0]);
-		})
-		//关
-		$('#filter-off').bind('click', function(){
-			source.removefilter();
-			$('#filter-off')[0].disabled = true;
-			$('#filter-on')[0].disabled = false;
-			for(var j = 0; j < 8; j++) {
-				$('#'+filter_arr[j][0])[0].disabled = false;
+		var that = this;
+		window.addEventListener('message', function(ev){
+			//console.log(ev.data);
+			var data = ev.data.split(',');
+			switch (data[0]){
+				case 'play':
+					stop = 0;
+					audio.play();
+					break;
+				case 'pause':
+					stop = 1;
+					audio.pause();
+					break;
+				case 'stop':
+					stop = 1;
+					audio.pause();
+					audio.currentTime = 0;
+					break;
+				case 'skip':
+					audio.currentTime += data[1];
+					break;
+				case 'vol':
+					source.setvol(data[1]);
+					break;
+				case 'addfilter':
+					source.addfilter();
+					break;
+				case 'removefilter':
+					source.removefilter();
+					break;
+				case 'filtertype':
+					source.setfilter.type(data[1]);
+					break;
+				case 'filterfrequency':
+					source.setfilter.frequency(parseInt(data[1]));
+					break;
+				case 'filterq':
+					source.setfilter.q(data[1]);
+					break;
+				case 'filtergain':
+					source.setfilter.gain(data[1]);
+					break;
+				default:
+					console.log('No this message!');
+					break;
 			}
 		})
-		//每个按钮的绑定
-		function fn(k){
-			$('#'+filter_arr[k][0]).bind('click', function(){
-				if(filter_arr[k][1] == 0)
-					filter_arr[k][1] = 1;
-				source.setfilter.type(filter_arr[k][0]);
-				for(var j = 0; j < 8; j++) {
-					if(k == j)
-						$('#'+filter_arr[j][0])[0].disabled = true;
-					else
-						$('#'+filter_arr[j][0])[0].disabled = false;
-				}
-			})
-		}
-		for(var i = 0; i < 8; i++) {
-			fn(i);
-		}
-		//下面三个控制条的绑定
-		$('#frequency').bind('change', function(){
-			var frequency_arr = [220,440,880,1600,3200,6400];
-			source.setfilter.frequency(frequency_arr[this.value]);
-		})
-		$('#q').bind('change', function(){
-			source.setfilter.q(this.value);
-		})
-		$('#filtergain').bind('change', function(){
-			source.setfilter.gain(this.value);
-		})
-
 	}
 	//执行此对象
 	,init: function(){
-		$('body')[0].style.height = document.documentElement.clientHeight;
 		this.startMusic();//音乐加载
-		this.startRub();//擦盘加载
-		this.drag();//拖拽绑定
-		this.rub();//擦盘实现
 		web_audio_api_init();//滤波器加载
 		this.control();//控制绑定
 	}
